f785b069d787e01172b5a9f1bca30ebb4991ab28
NS0296 <110940186+NS0296@users.noreply.github.com>
2025-11-17 19:59:42 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
-# entrypoint.sh
 #!/bin/bash
 set -e
 
-# 写入公钥到 authorized_keys（若提供）
+# 写入公钥到 authorized_keys
 if [ -n "$SSH_PUBLIC_KEY" ]; then
   mkdir -p /home/${SSH_USER}/.ssh
   echo "$SSH_PUBLIC_KEY" > /home/${SSH_USER}/.ssh/authorized_keys
@@ -10,8 +9,13 @@ if [ -n "$SSH_PUBLIC_KEY" ]; then
   chown -R ${SSH_USER}:${SSH_USER} /home/${SSH_USER}/.ssh
 fi
 
+# 写入证书内容到 stunnel.pem
+if [ -n "$STUNNEL_CERT" ]; then
+  echo "$STUNNEL_CERT" > /etc/stunnel/stunnel.pem
+fi
+
 # 启动 SSH
 service ssh start
 
-# 启动 stunnel（前台）
+# 启动 stunnel
 exec stunnel /etc/stunnel/stunnel.conf
